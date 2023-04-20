a9b64ecec1924a00801abbbe3554b9343f5b45ce
TanLoc <tanloc231102@gmail.com>
2023-04-20 16:04:15 +0700

    Temp

---
+++ main.js
@@ -0,0 +1,44 @@
+
+
+//Khai bao bien
+var fullName = 'Do Tan Loc'
+var age = '20'
+
+//Goi ham thong bao
+// alert(fullName)
+// alert(age)
+
+/**
+ * 
+ */
+
+var name = 'Gundam'
+
+// console.log('Day la 1 dong log ')
+// console.log(name)
+
+// confirm('Authorize')
+// prompt('Code to authorize')
+
+// setTimeout(function () {
+//     alert('Error')
+// }, 1000)
+
+// setInterval(function (){
+//     console.log('Warning' + Math.random())
+// }, 1000)
+
+var x = 1;
+console.log(x++)
+console.log(x)
+
+console.log(fullName + ' ' + age)
+
+var isSuc = true;
+console.log(isSuc);
+
+function showDiag(message) {
+    console.log(message);
+}
+
+showDiag('Do Tan Loc','20DTHC4');
